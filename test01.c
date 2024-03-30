91b54c8e14ba26f62b17bb5636bd9fc37f1457a0
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-03-30 15:47:08 -0600

    Funcion de procesos y Pila
    Se creó la funcion "conversion..." para comenzar con los procesos necesarios.
    Además, se creo la pila STACK que será necesaria.
    
    Se crearon dos funciones auxiliares para poder verificar de que tipo es el caracter que se está leyendo.

--- test01.c
+++ test01.c
@@ -9,20 +9,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 #define TAM 100
 #define TAM_CADENA 100
 
+struct STACK{
+    char stack[TAM]; 
+    char *ptrSt;
+};
+
 char *leerCadenaDesdeArchivo(char *nombreArchivo);
+void conversionInfijaPostfija(cadenaLeida);
+void escribirEnArchivo(char caracter, char *nombreArchivo);
+int esOperador(char caracter);
+int gradoOperador(char caracter);
 
 int main(){
 
     char *cadenaLeida = leerCadenaDesdeArchivo("entrada.txt");
+    char cadenaNueva[TAM];
 
     if (cadenaLeida != NULL) {
         printf("La cadena leída del archivo es: %s\n", cadenaLeida);
         free(cadenaLeida); // Liberar la memoria asignada para la cadena
     }
+
+    conversionInfijaPostfija(cadenaLeida);
 
     return 0;
 }
@@ -35,7 +48,7 @@ char *leerCadenaDesdeArchivo(char *nombreArchivo) {
     // Verifica si el archivo se abrió correctamente
     if (archivo == NULL) {
         printf("No se pudo abrir el archivo.\n");
-        return NULL; // Retorna NULL si hay un error
+        return NULL;
     }
 
     // Lee la cadena de texto del archivo
@@ -46,6 +59,57 @@ char *leerCadenaDesdeArchivo(char *nombreArchivo) {
         printf("El archivo está vacío.\n");
         fclose(archivo); // Cierra el archivo si está vacío
         free(cadena); // Libera la memoria asignada para la cadena
-        return NULL; // Retorna NULL si el archivo está vacío
+        return NULL;
+    }
+}
+
+// Función para administrar todo el procedimiento de conversión
+void conversionInfijaPostfija(cadenaLeida){
+
+    char *ptrCadena;
+
+    for(ptrCadena = cadenaLeida; *ptrCadena != '\0'; ptrCadena++){
+        if(isalpha(*ptrCadena))
+            escribirEnArchivo(*ptrCadena, "expresionPostfija.txt");
+        else if(esOperador(*ptrCadena)){
+            int grado = gradoOperador(*ptrCadena);
+        }
+
+            
+    }
+
+}
+
+void escribirEnArchivo(char caracter, char *nombreArchivo) {
+    FILE *archivo = fopen(nombreArchivo, "a"); // Abre el archivo en modo de escritura, agregando al final del archivo
+
+    // Verifica si el archivo se abrió correctamente
+    if (archivo == NULL) {
+        printf("No se pudo abrir el archivo.\n");
+        return; // Termina la función si hay un error
+    }
+
+    // Escribe el caracter en el archivo
+    fprintf(archivo, "%c", caracter);
+
+    // Cierra el archivo
+    fclose(archivo);
+}
+
+int esOperador(char caracter){
+    return (caracter == '+' || caracter == '-' || caracter == '*' || caracter == '/' || caracter == '^') ? 1 : 0;
+}
+int gradoOperador(char caracter) {
+    switch(caracter) {
+        case '^':
+            return 3;
+        case '*':
+        case '/':
+            return 2;
+        case '+':
+        case '-':
+            return 1;
+        default:
+            return 0; 
     }
 }
